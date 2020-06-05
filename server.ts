6bece33984c05797cef649708ce262db403d6066
Trung Hieu <trunghieu2570@gmail.com>
2020-06-05 11:46:29 +0700

    remove env

--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
 import "module-alias/register";
-import dotenv from "dotenv";
 import App from "./src/app";
 
 
 (async function startServer() {
-
-  dotenv.config();
 
   const host = process.env.HOST || "localhost";
   const port = parseInt(process.env.PORT, 10) || 3000;
