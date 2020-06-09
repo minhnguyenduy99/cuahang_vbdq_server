b7aa383acd9b9b150a501dfc61c3b1f0c3496e24
minhnguuyenduy <minhnguyenduy99@gmail.com>
2020-06-09 10:16:31 +0700

    Add NhanVien and KhachHang initializers

--- src/settings/initializeModule.ts
+++ src/settings/initializeModule.ts
@@ -1,6 +1,13 @@
 import { initialize as initializeRole } from "@modules/roles/shared";
+import { initializer as khachhangInitializer } from "@modules/khachhang/shared";
+import { initializer as nhanvienInitializer } from "@modules/nhanvien/shared";
+
 import { Dependency } from "@dep";
 
-export default function initializeModule(dep: Dependency) {
-  initializeRole(dep);
+export default async function initializeModule(dep: Dependency) {
+  await Promise.all([
+    initializeRole(dep),
+    khachhangInitializer.initialize(dep),
+    nhanvienInitializer.initialize(dep)
+  ])
 }
